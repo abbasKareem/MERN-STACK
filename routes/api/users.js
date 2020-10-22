aa74988e7838c12dd9ff2bc611e45813ea48bd6b
abbas <kareemabbas106@gmail.com>
2020-10-22 22:27:10 +0200

    sending user data to the database (create a user)

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,62 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator");
 
-// @route: GET api/users
-//@desc: Test route
+const User = require("../../models/User");
+
+// @route: POSt api/users
+//@desc: Register route
 //@access Public
-router.get('/', (req, res) => res.send('User route'));
+router.post(
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({
+      min: 6,
+    }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      //SEE IF USER EXISTES
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exist" }] });
+      }
+      //GET USERS GARAVATAR
+      const avatar = gravatar.url(email, { s: "200", r: "bg", d: "mm" });
+      //Greate an instance of the user (object)
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+      //ENCRYPT PASSWORD
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+      //RETURN JSON WEBTOKEN
+      res.send("User registered");
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
 
 module.exports = router;
