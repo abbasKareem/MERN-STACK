126b5a1633fbd7239d4cf8f2c1ae8d3e7ad587f7
abbas <kareemabbas106@gmail.com>
2020-11-06 14:14:09 +0200

    start implementing React and install dependence

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import "./App.css";
+
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+
+const App = () => (
+  <Fragment>
+    <Navbar />
+    <Landing />
+  </Fragment>
+);
+
+export default App;
