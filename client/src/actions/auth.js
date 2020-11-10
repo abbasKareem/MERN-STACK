2cf542006e835276ad721150242aef6a0b39a19c
abbas <kareemabbas106@gmail.com>
2020-11-10 20:10:30 +0200

    Starting On The Dashboard

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -8,6 +8,7 @@ import {
   LOGIN_SUCCESS,
   LOGIN_FAIL,
   LOGOUT,
+  CLEAR_PROFILE,
 } from "./types";
 import setAuthToken from "../utils/setAuthToken";
 
@@ -89,6 +90,9 @@ export const login = (email, password) => async (dispatch) => {
 
 //Logout / Clear Profile
 export const logout = () => (dispatch) => {
+  dispatch({
+    type: CLEAR_PROFILE,
+  });
   dispatch({
     type: LOGOUT,
   });
